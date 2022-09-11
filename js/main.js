b6f65b9705c4ccdc0cd25eebdefe3bca05dc69b0
daryana69 <113286513+daryana69@users.noreply.github.com>
2022-09-11 13:26:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector('#menuicon')
+const close = document.querySelector('.close')
+const nav = document.querySelector('nav')
+
+// console.log(menu)
+// console.log(close)
+// console.log(nav)
+
+menu.addEventListener('click', () => {
+    nav.classList.add('open-nav')
+})
+
+close.addEventListener('click', () => {
+    nav.classList.remove('open-nav')
+})
